4de228aea31b1c6eeffa4c976c08fffc8bb09c94
HectorArcega <818058@unizar.es>
2023-12-03 20:56:32 +0100

    CsvController.kt
    Código del controlador de csv sin implementar en la web ni probarlo

---
+++ delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/CsvController.kt
@@ -0,0 +1,61 @@
+package es.unizar.urlshortener.infrastructure.delivery
+
+import es.unizar.urlshortener.core.usecases.CsvUserCase
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+
+
+interface CsvController {
+
+    /**
+     * Genera y devuelve un csv en funcion del csv de entrada y del texto personalizado(aun no)
+     */
+    fun generateCsv(
+    @RequestPart("csvFile") csvFile: MultipartFile,
+    @RequestPart("customText") customText: String = ""
+): ResponseEntity<String>
+}
+
+/**
+ * Implementacion del controlador csv
+ */
+@RestController
+class CsvControllerImpl(
+    private val csvUserCase: CsvUserCase
+) : CsvController {
+
+    @PostMapping("/api/bulk", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    override fun generateCsv(
+        @RequestPart("csvFile") csvFile: MultipartFile,
+        @RequestPart("customText") customText: String
+    ): ResponseEntity<String> {
+        // Convertir MultipartFile a File
+        val convertedCsvFile = convertMultipartFileToFile(csvFile)
+
+        // Llamar al caso de uso para generar el CSV
+        val csvContent = csvUserCase.createCsv(convertedCsvFile, customText)
+
+        // Configurar la respuesta HTTP
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.parseMediaType("text/csv")
+        headers.setContentDispositionFormData("attachment", "output.csv")
+
+        // Devolver la respuesta HTTP con el contenido del CSV
+        return ResponseEntity(csvContent, headers, HttpStatus.OK)
+    }
+
+    // Metodo para convertir MultipartFile a File
+    private fun convertMultipartFileToFile(multipartFile: MultipartFile): File {
+        val file = File(multipartFile.originalFilename!!)
+        multipartFile.transferTo(file)
+        return file
+    }
+}
