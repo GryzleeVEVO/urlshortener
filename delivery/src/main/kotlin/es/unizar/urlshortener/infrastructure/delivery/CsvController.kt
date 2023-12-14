0e1ad61ed32d38242070c0b55810fabb4f0864d6
HectorArcega <818058@unizar.es>
2023-12-14 13:26:16 +0100

    Location con 1ª url acortada

--- delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/CsvController.kt
+++ delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/CsvController.kt
@@ -14,9 +14,14 @@ import java.io.File
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.hateoas.server.mvc.linkTo
 
+import es.unizar.urlshortener.core.ClickProperties
+import org.springframework.web.bind.annotation.PathVariable
+// import es.unizar.urlshortener.core.usecases.LogClickUseCase
+// import es.unizar.urlshortener.core.usecases.RedirectUseCase
+import java.net.URI
 
 interface CsvController {
-
+    //fun redirectTo(id: String, request: HttpServletRequest): ResponseEntity<Unit>
     /**
      * Genera y devuelve un csv en funcion del csv de entrada y del texto personalizado(aun no)
      */
@@ -26,8 +31,7 @@ interface CsvController {
     fun generateCsv(
     @RequestPart("csvFile") csvFile: MultipartFile,
     @RequestPart("customText") customText: String?,
-    request: HttpServletRequest
-    
+    request: HttpServletRequest    
 ): ResponseEntity<String>
 }
 
@@ -40,6 +44,16 @@ interface CsvController {
 class CsvControllerImpl(
     private val csvUseCase: CsvUseCase
 ) : CsvController {
+
+    // @PostMapping("/{id:(?!api|index).*}")
+    // override fun redirectTo(@PathVariable id: String, request: HttpServletRequest): ResponseEntity<Unit> =
+    //     redirectUseCase.redirectTo(id).let {
+    //         logClickUseCase.logClick(id, ClickProperties(ip = request.remoteAddr))
+    //         val h = HttpHeaders()
+    //         h.location = URI.create(it.target)
+    //         ResponseEntity<Unit>(h, HttpStatus.valueOf(it.mode))
+    //     }
+
     @PostMapping("/api/bulk", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     override fun generateCsv(
         @RequestPart("file") csvFile: MultipartFile,
@@ -73,7 +87,7 @@ class CsvControllerImpl(
         //     println(item)
         // }
         
-        //ESTE ES EL BUENO
+        //ESTE ES EL BUENO, poner la ip a los hash
         val csvContentWithFullUrls = cadenaResultado.lines().map { line ->
             val splitLine = line.split(",")
             if (splitLine.size >= 2) {
@@ -86,13 +100,22 @@ class CsvControllerImpl(
                 "Invalid line format: $line"
             }
         }.joinToString("\n")
+
+        //coger la primera url acortada para la cabecera location
+        val lines = csvContentWithFullUrls.trim().split("\n")
+        var firstShortenedUrl: String = ""
+        for (line in lines) {
+            firstShortenedUrl = line.substringAfter(",")
+            println("Primera URL acortada: $firstShortenedUrl")
+            break
+        }
         
 
         // Configurar la respuesta HTTP
         val headers = HttpHeaders()
         headers.contentType = MediaType.parseMediaType("text/csv")
         headers.setContentDispositionFormData("attachment", "output.csv")
-
+        headers.location = URI.create(firstShortenedUrl)
             // Devolver la respuesta HTTP con el contenido del CSV
         return ResponseEntity(csvContentWithFullUrls, headers, HttpStatus.CREATED)
 
